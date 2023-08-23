18095a66e4af3ccd54e4b3494b30ec739f264c08
domm <dommferger@gmail.com>
2023-08-23 09:26:57 -0400

    updated

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function addingEventListener() {
     
     input.addEventListener('click', clickAlert);
 }
+
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
